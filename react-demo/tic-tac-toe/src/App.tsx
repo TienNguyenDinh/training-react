0554b6ad4df4b2b97d7e8114b3506a4193c747f3
Tien Nguyen <mrfuree@gmail.com>
2024-03-13 13:38:10 +0700

    feat(app): finish tic-tac-toe

--- react-demo/tic-tac-toe/src/App.tsx
+++ react-demo/tic-tac-toe/src/App.tsx
@@ -1,11 +1,45 @@
-import { useState } from 'react'
-import './App.css'
+import React, { useState } from 'react';
+import './App.css';
 
-const App = () => {
-  return (
-    <>
-    </>
-  )
+type SquareValue = 'X' | 'O' | 'null';
+
+interface SquareProps {
+  value: SquareValue;
+  onClick: () => void;
 }
 
-export default App
+const Square: React.FC<SquareProps> = ({ value, onClick }) => (
+  <button className='square' onClick={onClick}>
+    {value}
+  </button>
+);
+
+const App = () => {
+  const [squares, setSquares] = useState<Array<SquareValue>>(Array(9).fill(''));
+  const [isXNext, setIsXNext] = useState<boolean>(true);
+
+  const handleClick = (i: number) => {
+    const squaresCopy = squares.slice();
+    if (squaresCopy[i]) {
+      return;
+    }
+    
+    squaresCopy[i] = isXNext ? 'X' : 'O';
+    setSquares(squaresCopy);
+    setIsXNext(!isXNext);
+  };
+
+  return (
+    <div className='table-layout'>
+      {squares.map((square, i) => (
+        <Square
+          key={i}
+          value={square}
+          onClick={() => handleClick(i)}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default App;
