b5bb3853fc8060470df082297e3ca0e6632e63c9
Tien Nguyen <mrfuree@gmail.com>
2024-03-13 10:44:28 +0700

    feat(app): finish counter app

--- react-demo/counter-app/src/App.tsx
+++ react-demo/counter-app/src/App.tsx
@@ -1,11 +1,18 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from 'react';
 
 const App = () => {
-  return (
-    <>
-    </>
-  )
-}
+  const [count, setCount] = useState<number>(0);
 
-export default App
+  const increment = () => setCount(prevCount => prevCount + 1);
+  const decrement = () => setCount(prevCount => prevCount - 1);
+
+  return (
+    <div>
+      <h1>Counter: {count}</h1>
+      <button onClick={increment}>Increment</button>
+      <button onClick={decrement}>Decrement</button>
+    </div>
+  );
+};
+
+export default App;
