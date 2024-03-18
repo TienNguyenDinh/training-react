08d2c111daaf858c66be9e0b6823a661edbc80b7
Tien Nguyen <mrfuree@gmail.com>
2024-03-18 16:17:27 +0700

    feat(app): finish useEffect demo

--- react-demo/useEffect/src/App.tsx
+++ react-demo/useEffect/src/App.tsx
@@ -1,10 +1,38 @@
-import { useState } from 'react'
-import './App.css'
+import { useState, useEffect } from 'react';
+import './App.css';
+
+interface Data {
+  id: string,
+  userId: string,
+  title: string,
+  completed: boolean
+}
 
 const App = () => {
+  const [count, setCount] = useState<number>(0);
+  const [data, setData] = useState<Data[]>([]);
+  
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCount(c => c + 1);
+    }, 1000);
+
+    return () => {
+      clearInterval(interval);
+    }
+  }, []);
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/todos')
+      .then(res => res.json())
+      .then((data: Data[]) => setData(data));
+  }, []);
+
   return (
-    <>
-    </>
+    <div key={count}>
+      <p>Count: {count}</p>
+      <p>Data length: {data.length}</p>
+    </div>
   )
 }
 
