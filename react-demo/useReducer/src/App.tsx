2216e01cf44454393e1de1843b3dd1e36b0cfe11
Tien Nguyen <mrfuree@gmail.com>
2024-03-18 17:41:32 +0700

    feat(app): building useReducer demo

--- react-demo/useReducer/src/App.tsx
+++ react-demo/useReducer/src/App.tsx
@@ -1,7 +1,37 @@
-import { useState } from 'react'
-import './App.css'
+import { useReducer } from 'react';
+import './App.css';
+
+type Action = 
+
+interface Payload {
+  value: number
+}
+
+const countReducer = (state: number, action: Action) => {
+  const { payload } = action;
+
+  switch(action.type) {
+    case 'INCREMENT': {
+      return state + 1;
+    }
+    case 'DECREMENT': {
+      return state - 1;
+    }
+    case 'PLUS': {
+      return state + payload.value;
+    }
+    case 'MINUS': {
+      return state - payload.value;
+    }
+    case 'RESET': {
+      return 0;
+    }
+  }
+}
 
 const App = () => {
+  const [count, dispatchCount] = useReducer(countReducer, 0);
+
   return (
     <>
     </>
