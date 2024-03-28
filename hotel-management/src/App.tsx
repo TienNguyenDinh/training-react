d703d8fb3c07792c5061d7e1948f7f1d72f0b71e
Tien Nguyen <mrfuree@gmail.com>
2024-03-28 10:27:37 +0700

    feat(script): install and config prettier

--- hotel-management/src/App.tsx
+++ hotel-management/src/App.tsx
@@ -1,11 +1,8 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
 
 const App = () => {
-  return (
-    <>
-    </>
-  )
-}
+  return <></>;
+};
 
-export default App
+export default App;
